810ae606c32ac149ebe00f6578085aed8786ce74
Khamidov-M <mardonbekkhamidov69@gmail.com>
2024-02-29 22:16:54 +0500

    fix

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-contain ">
+        <p className="text-6xl text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <div className="w-full flex justify-center">
-      <div className="w-contain"></div>
+      <div className="w-contain">
+        <Hero />
+      </div>
     </div>
   );
 };
