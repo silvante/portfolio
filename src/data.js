c56be794b98ea7f9283cc4bf8029ce6c54e2078d
Khamidov-M <mardonbekkhamidov69@gmail.com>
2024-03-02 18:09:55 +0500

    upgrage

---
+++ src/data.js
@@ -0,0 +1,74 @@
+import html from "./assets/html-1.svg";
+import css from "./assets/css-3.svg";
+import js from "./assets/logo-javascript.svg";
+import bootstrap from "./assets/bootstrap-5-1.svg";
+import tailwind from "./assets/tailwindcss.svg";
+import git from "./assets/git-icon.svg";
+import github from "./assets/github-icon-1.svg";
+import mui from "./assets/material-ui-1.svg";
+import react from "./assets/react.svg";
+
+export const skills = [
+  {
+    id: 1,
+    name: "HTML",
+    logo: html,
+    info: "using html 5",
+    star: 5,
+  },
+  {
+    id: 2,
+    name: "CSS",
+    logo: css,
+    info: "using css 3",
+    star: 4,
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    logo: js,
+    info: "programming language",
+    star: 3,
+  },
+  {
+    id: 4,
+    name: "React",
+    logo: react,
+    info: "using lastest",
+    star: 4,
+  },
+  {
+    id: 5,
+    name: "Tailwind css",
+    logo: tailwind,
+    info: "css framework",
+    star: 5,
+  },
+  {
+    id: 6,
+    name: "git",
+    logo: git,
+    info: "github halper",
+    star: 4,
+  },
+  {
+    id: 7,
+    name: "GitHub",
+    logo: github,
+    info: "nick: Khamidov-M",
+    star: 5,
+  },
+  {
+    id: 8,
+    name: "MUi",
+    logo: mui,
+    info: "Material ui",
+    star: 3,
+  },
+  {
+    id: 9,
+    name: "Bootsrap",
+    logo: bootstrap,
+    info: "not often using",
+  },
+];
