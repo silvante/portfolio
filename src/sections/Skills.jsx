503da1a89e00c8ade69440be6f05c4f21c77fa88
Khamidov-M <mardonbekkhamidov69@gmail.com>
2024-03-02 18:54:45 +0500

    skills page is ready

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { skills } from "../data";
-// import { Rating } from "@mui/material";
+import { Rating } from "@mui/material";
 
 const Skills = () => {
+  const starColor = "black";
   return (
     <div className="w-full flex justify-center">
       <div className="w-contain text-center space-y-5">
         <h2 className="text-2xl font-bold">Skills</h2>
         <p>I work in programs like</p>
-        <ul className="flex flex-wrap justify-center items-start gap-5">
+        <ul className="flex flex-wrap justify-center items-start gap-10">
           {skills.map((skill) => {
             return (
               <li className="w-[150px]" key={skill.id}>
@@ -20,10 +21,18 @@ const Skills = () => {
                     height={"100px"}
                   />
                 </div>
-                <div>
-                  <p>{skill.name}</p>
+                <div className="space-y-3">
+                  <div>
+                    <p>{skill.name}</p>
+                  </div>
+                  <Rating
+                    name="read-only"
+                    value={skill.star}
+                    className=""
+                    readOnly
+                    style={{ color: starColor }}
+                  />
                 </div>
-                {/* <Rating name="read-only" value={skill.star} className="" readOnly /> */}
               </li>
             );
           })}
