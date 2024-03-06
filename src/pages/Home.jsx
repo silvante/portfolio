d4aa263dd45fb103d87045e3c823c0e16f49d8df
Khamidov-M <mardonbekkhamidov69@gmail.com>
2024-03-06 14:06:08 +0500

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,25 +5,12 @@ import Skills from "../sections/Skills";
 import Portfolio from "../sections/Portfolio";
 
 const Home = () => {
-  const scrollRef = useRef(null);
-
-  const handleScroll = () => {
-    if (scrollRef.current) {
-      const scrollIndex = scrollRef.current.scrollTop;
-      console.log("Scroll Index:", scrollIndex);
-      // Do something with the scroll index
-    }
-  };
   return (
     <div className="space-y-10">
       <Hero />
       <AboutMe />
       <Skills />
-      <Portfolio
-        ref={scrollRef}
-        style={{ height: "300px", overflowY: "scroll" }}
-        onScroll={handleScroll}
-      />
+      <Portfolio />
     </div>
   );
 };
