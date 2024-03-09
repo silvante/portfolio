17b0237e5fa2113727e33fa178f43a7a753bb616
Khamidov-M <mardonbekkhamidov69@gmail.com>
2024-03-09 17:51:05 +0500

    responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,13 @@ const Hero = () => {
   return (
     <div className="w-full flex justify-center">
       <div className="w-contain bg-img rounded overflow-hidden">
-        <div className="w-full h-full py-10 px-10 bg-black/70 flex justify-between items-center">
-          <h1 className="text-white w-[50%] text-4xl font line-h">
+        <div className="w-full h-full py-10 px-10 bg-black/70 space-y-5 flex flex-col md:flex-row text-center justify-between items-center">
+          <h1 className="text-white w-[100%] md:w-[50%] text-2xl font md:text-start sm:text-4xl sm:leading-tight">
             Your <span className="text-black bg-white">misstakes</span> will
             grow you, from zero to{" "}
             <span className="text-black bg-white">hero</span>
           </h1>
-          <div className="w-[50%] flex justify-end">
+          <div className="w-[100%] md:w-[50%] flex justify-center md:justify-end">
             <img src={pfp} alt="my image" width={"300px"} height={"300px"} className="rounded-full border-2 border-white"/>
           </div>
         </div>
