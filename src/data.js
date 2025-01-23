59a203e766aac99d37d21bc937d397bb851e80df
silvante <khamidov.ko@gmail.com>
2025-01-23 13:48:27 +0500

    new projects

--- src/data.js
+++ src/data.js
@@ -10,12 +10,14 @@ import react from "./assets/react.svg";
 import express from "./assets/express.svg";
 import mongodb from "./assets/mongodb.svg";
 import nodejs from "./assets/nadejs.svg";
+import rails from "./assets/rails.svg";
+import ruby from "./assets/Ruby_logo.png";
+import mysql from "./assets/mysql.svg"
 
 import autoposter from "./assets/pr/autoposter.png";
-import cuser from "./assets/pr/cuser.png";
 import mcg from "./assets/pr/mcg.png";
 import slivait from "./assets/pr/slivait.png";
-import apprest from "./assets/pr/apprest.png";
+import menemarket from "./assets/pr/menemarket.png"
 
 export const skills = [
   {
@@ -114,6 +116,30 @@ export const skills = [
     star: 3,
     isLearning: false,
   },
+  {
+    id: 13,
+    name: "Ruby",
+    logo: ruby,
+    info: "for personal use",
+    star: 3,
+    isLearning: true,
+  },
+  {
+    id: 14,
+    name: "Ruby on Rails",
+    logo: rails,
+    info: "for Full-stack",
+    star: 5,
+    isLearning: true,
+  },
+  {
+    id: 15,
+    name: "Mysql",
+    logo: mysql,
+    info: "with ORM",
+    star: 4,
+    isLearning: true,
+  },
 ];
 
 export const projects = [
@@ -129,16 +155,6 @@ export const projects = [
   },
   {
     id: 2,
-    name: "CUser",
-    info: "CUser is simple react example for learning, it was created for fun.",
-    link: "https://cuser-web.netlify.app/",
-    gitHub: "https://github.com/Khamidov-M/my-team",
-    image: cuser,
-    texnos: ["#html", "#css", "#JavaScript", "#React"],
-    star: 1,
-  },
-  {
-    id: 3,
     name: "MCG",
     info: "this game wes created when I was just learning begining of js, for fun.",
     link: "https://mcg-game.netlify.app/",
@@ -148,23 +164,23 @@ export const projects = [
     star: 2,
   },
   {
-    id: 4,
+    id: 3,
     name: "Slivait",
     info: "This web was created for fun when I was learning node js and express (mern)",
     link: "https://slivait.netlify.app/",
     gitHub: "https://github.com/silvante/slivait",
     image: slivait,
     texnos: ["#html", "#css", "#JavaScript", "#react", "#tailwind"],
-    star: 4,
+    star: 2,
   },
   {
-    id: 5,
-    name: "Apprest (restfull)",
-    info: "I created this minimal functional App for learning mern stack",
-    link: "https://apprest.netlify.app/",
-    gitHub: "https://github.com/silvante/apprest",
-    image: apprest,
-    texnos: ["#client", "#backend"],
+    id: 4,
+    name: "mene_market backend",
+    info: "Backend part of mene market e-comm",
+    link: "https://menemarket.netlify.app/",
+    gitHub: "https://github.com/silvante/mene_market",
+    image: menemarket,
+    texnos: ["#node", "#express", "#jwt", "#mongodb"],
     star: 5,
   },
 ];
