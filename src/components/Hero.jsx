369ad30734f684c8425bf12e3d4316f4d113dec1
silvante <khamidov.ko@gmail.com>
2025-02-07 21:54:03 +0500

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,8 +36,8 @@ const Hero = () => {
         <div className="bg-img rounded overflow-hidden">
           <div className="w-full h-full py-10 px-10 bg-black/70 space-y-5 flex flex-col md:flex-row text-center justify-between items-center">
             <h1 className="text-white w-[100%] md:w-[50%] text-2xl font md:text-start sm:text-4xl sm:leading-tight">
-              Your <span className="text-black bg-white">misstakes</span> will
-              grow you, from zero to{" "}
+              Your <span className="text-black bg-white">mistakes</span> will
+              grow you from zero to{" "}
               <span className="text-black bg-white">hero</span>
             </h1>
             <div className="w-[100%] md:w-[50%] flex justify-center md:justify-end">
