a8783785c0a4df2335e4222aadb7f8f92fe4c55d
silvante <khamidov.ko@gmail.com>
2025-03-20 22:37:14 -0400

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {
       width: {
@@ -8,5 +10,5 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
