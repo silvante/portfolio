306c12dfc33f707ca1344014e9832406f68c32fc
silvante <khamidov.ko@gmail.com>
2025-05-30 21:43:46 +1300

    op settings

--- src/sections/Articles.jsx
+++ src/sections/Articles.jsx
@@ -3,6 +3,7 @@ import { articles } from "../data";
 import At from "../components/At";
 
 const Articles = () => {
+  const re_articles = [...articles].reverse()
   return (
     <div className="w-full">
       <div className="flex w-full justify-between items-center">
@@ -10,7 +11,7 @@ const Articles = () => {
         <p>My Ideas and phylosophy</p>
       </div>
       <div className="w-full py-5 space-y-5">
-        {articles.map((ar) => {
+        {re_articles.map((ar) => {
           return (
             <div key={ar.id} className="space-y-3">
               <At text={ar.at} />
