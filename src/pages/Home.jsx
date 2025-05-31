3dc8e187342c98886ab7967caf41b83ef8ce132c
silvante <khamidov.ko@gmail.com>
2025-06-01 06:31:59 +1300

    resume button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Banner from "../components/Banner";
-import Space from "../lottie/Space";
 import Articles from "../sections/Articles";
+import Resume from "../assets/resume/xamidov_resume.pdf"
+import { FileDown } from "lucide-react";
 
 const Home = () => {
   return (
     <div className="p-5">
       <Banner />
+      <div className="w-full flex justify-center items-center">
+        <a href={Resume} download className="flex gap-1 bg-violet-600 py-2 px-4 rounded-xl" >Download Resume <FileDown /></a>
+      </div>
       <Articles />
       <div className="w-full flex justify-center">
         <div className="">
