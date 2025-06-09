06db10b98350daae2e2e956241769e8ce10ab72a
silvante <khamidov.ko@gmail.com>
2025-06-10 03:40:27 +0500

    album media

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -29,24 +29,30 @@ const Album = () => {
         </div>
       )}
 
-      <div className="w-full grid grid-cols-1 gap-3 md:grid-cols-2">
-        <div>
-          <img src={main_pic.photo} alt="main image" onLoad={handleImageLoad} />
+      {all_loaded && (
+        <div className="w-full grid grid-cols-1 gap-3 md:grid-cols-2">
+          <div>
+            <img
+              src={main_pic.photo}
+              alt="main image"
+              onLoad={handleImageLoad}
+            />
+          </div>
+          <div className="grid grid-cols-2 grid-rows-2 gap-3">
+            {filtered_photos.map((p) => {
+              return (
+                <button onClick={() => setIndex(p.id)} key={p.id}>
+                  <img
+                    src={p.photo}
+                    alt={`my image ${p.id}`}
+                    onLoad={handleImageLoad}
+                  />
+                </button>
+              );
+            })}
+          </div>
         </div>
-        <div className="grid grid-cols-2 grid-rows-2 gap-3">
-          {filtered_photos.map((p) => {
-            return (
-              <button onClick={() => setIndex(p.id)} key={p.id}>
-                <img
-                  src={p.photo}
-                  alt={`my image ${p.id}`}
-                  onLoad={handleImageLoad}
-                />
-              </button>
-            );
-          })}
-        </div>
-      </div>
+      )}
     </>
   );
 };
