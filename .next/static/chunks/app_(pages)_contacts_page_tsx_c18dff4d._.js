68563173d1ef4bc959889cd154cb4ae38e75c179
silvante <khamidov.ko@gmail.com>
2025-06-21 18:02:38 +0500

    switching errors

--- .next/static/chunks/app_(pages)_contacts_page_tsx_c18dff4d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_918cd437._.js",
-  "static/chunks/app_(sections)_Contact_tsx_671548c2._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_page_tsx_c18dff4d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_81ebaa0b._.js",
-  "static/chunks/node_modules_95463b29._.js"
-],
-    source: "dynamic"
-});
